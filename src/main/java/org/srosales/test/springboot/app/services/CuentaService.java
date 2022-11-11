87c1456569c4d59d2e5662b36549a7e62480e593
SRubiRosales <sharon_rosales98@hotmail.com>
2022-11-11 09:34:30 -0600

    Test con JUnit y Mockito

--- src/main/java/org/srosales/test/springboot/app/services/CuentaService.java
+++ src/main/java/org/srosales/test/springboot/app/services/CuentaService.java
@@ -8,5 +8,5 @@ public interface CuentaService {
     Cuenta findById(Long id);
     int revisarTotalTransferencias(Long bancoId);
     BigDecimal revisarSaldo(Long cuentaId);
-    void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto);
+    void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto, Long bancoId);
 }
