0ae2226679c7db554b77f1c322a7d562c549e204
SRubiRosales <sharon_rosales98@hotmail.com>
2022-11-30 09:47:40 -0600

    Controlador Cuenta agregado

---
+++ src/main/java/org/srosales/test/springboot/app/controllers/CuentaController.java
@@ -0,0 +1,24 @@
+package org.srosales.test.springboot.app.controllers;
+
+import static org.springframework.http.HttpStatus.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.srosales.test.springboot.app.models.Cuenta;
+import org.srosales.test.springboot.app.services.CuentaService;
+
+@Controller
+@RequestMapping("/api/cuentas")
+public class CuentaController {
+    @Autowired
+    private CuentaService cuentaService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(OK)
+    public Cuenta detail(@PathVariable(name = "id") Long id) {
+        return cuentaService.findById(id);
+    }
+}
