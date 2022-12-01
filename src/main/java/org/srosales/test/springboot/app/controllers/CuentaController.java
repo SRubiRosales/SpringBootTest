86079497f748910d1874f73b6305a950e9d543a5
SRubiRosales <sharon_rosales98@hotmail.com>
2022-12-01 09:52:13 -0600

    Configuracion Swagger

--- src/main/java/org/srosales/test/springboot/app/controllers/CuentaController.java
+++ src/main/java/org/srosales/test/springboot/app/controllers/CuentaController.java
@@ -3,7 +3,6 @@ package org.srosales.test.springboot.app.controllers;
 import static org.springframework.http.HttpStatus.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.srosales.test.springboot.app.models.Cuenta;
 import org.srosales.test.springboot.app.models.TransaccionDto;
@@ -13,7 +12,7 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/api/cuentas")
 public class CuentaController {
     @Autowired
