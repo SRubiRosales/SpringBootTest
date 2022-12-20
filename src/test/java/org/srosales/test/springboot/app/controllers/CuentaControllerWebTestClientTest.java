2809e7d77707e8397751f3952edb8732e66e50e4
SRubiRosales <sharon_rosales98@hotmail.com>
2022-12-20 09:50:40 -0600

    Test de integración para metodo guardar

--- src/test/java/org/srosales/test/springboot/app/controllers/CuentaControllerWebTestClientTest.java
+++ src/test/java/org/srosales/test/springboot/app/controllers/CuentaControllerWebTestClientTest.java
@@ -108,6 +108,7 @@ class CuentaControllerWebTestClientTest {
                 .expectBody(Cuenta.class)
                 .consumeWith(response -> {
                     Cuenta cuenta = response.getResponseBody();
+                    assertNotNull(cuenta);
                     assertEquals("Rubí", cuenta.getPersona());
                     assertEquals("2100.00", cuenta.getSaldo().toPlainString());
                 });
@@ -155,5 +156,48 @@ class CuentaControllerWebTestClientTest {
                 // Otras formas de validar el tamaño de la respuesta
                 .hasSize(2)
                 .value(hasSize(2));
+    }
+
+    @Test
+    @Order(6)
+    void testGuardar() {
+        // Given
+        Cuenta cuenta = new Cuenta(null, "Pepe", new BigDecimal("3000"));
+        // When
+        client.post().uri("/api/cuentas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(cuenta)
+                .exchange()
+        // Then
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(3)
+                .jsonPath("$.persona").isEqualTo("Pepe")
+                .jsonPath("$.persona").value(is("Pepe"))
+                .jsonPath("$.saldo").isEqualTo(3000);
+    }
+
+    @Test
+    @Order(7)
+    void testGuardar2() {
+        // Given
+        Cuenta cuenta = new Cuenta(null, "Pepa", new BigDecimal("3500"));
+        // When
+        client.post().uri("/api/cuentas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(cuenta)
+                .exchange()
+                // Then
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(Cuenta.class)
+                .consumeWith(response -> {
+                    Cuenta c = response.getResponseBody();
+                    assertNotNull(c);
+                    assertEquals(4, c.getId());
+                    assertEquals("Pepa", c.getPersona());
+                    assertEquals("3500", c.getSaldo().toPlainString());
+                });
     }
 }
